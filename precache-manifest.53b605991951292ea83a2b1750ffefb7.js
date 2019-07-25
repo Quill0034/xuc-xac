a73b2c20fd870b36ddd9d011752921eace70ed6c
Quil <quy.tran@aceproject.space>
2019-07-24 23:46:43 -0500

    Updates

---
+++ precache-manifest.53b605991951292ea83a2b1750ffefb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696d90f478edd1e0ec2958992865701d",
+    "url": "/xuc-xac/index.html"
+  },
+  {
+    "revision": "a255f6b25312bd9fe40c",
+    "url": "/xuc-xac/static/css/main.a52812f4.chunk.css"
+  },
+  {
+    "revision": "c4a672ef54891a29a24c",
+    "url": "/xuc-xac/static/js/2.10e9addd.chunk.js"
+  },
+  {
+    "revision": "a255f6b25312bd9fe40c",
+    "url": "/xuc-xac/static/js/main.098b0b35.chunk.js"
+  },
+  {
+    "revision": "a0b167d90b767c78aaeb",
+    "url": "/xuc-xac/static/js/runtime~main.aea45ac0.js"
+  }
+]);
